da54d5a13182095f0a5f03cbc31815582a78fc8b
Shammika Wijesinghe <102298368+shux360@users.noreply.github.com>
2024-01-28 21:18:27 +0530

    adding the teacher page

---
+++ src/renderer/pages/dashboard/teacher/index.tsx
@@ -0,0 +1,71 @@
+import Sidebar from '@/components/sidebar';
+import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
+
+import { Menu } from 'lucide-react';
+import { columns } from './columns';
+import { CreateTeacherModal } from './create-teacher-modal';
+import { useState } from 'react';
+
+const data = [
+  {
+    id: '1',
+    firstName: 'Nguyen',
+    lastName: 'Van A',
+    birthday: '01/01/1990',
+    phone: '0123456789',
+    address: 'Hanoi',
+  },
+  {
+    id: '2',
+    firstName: 'aaa',
+    lastName: 'Van A',
+    birthday: '01/01/1990',
+    phone: '0123456789',
+    address: 'Hanoi',
+  },
+  {
+    id: '3',
+    firstName: 'cc',
+    lastName: 'Van A',
+    birthday: '01/01/1990',
+    phone: '0123456789',
+    address: 'Hanoi',
+  },
+  {
+    id: '4',
+    firstName: 'Ngttbuyen',
+    lastName: 'Van A',
+    birthday: '01/01/1990',
+    phone: '0123456789',
+    address: 'Hanoi',
+  },
+];
+
+function Teacher() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="container">
+      <div className="flex flex-row items-center justify-between py-10">
+        <div className="flex flex-row justify-left space-x-5 text-3xl text-black">
+          <Sidebar />
+          <h1>Teacher</h1>
+        </div>
+        <Button onClick={() => setOpen(true)}>New</Button>
+      </div>
+
+      <DataTable data={data} columns={columns} searchKey="id" />
+
+      <CreateTeacherModal
+        open={open}
+        onSubmit={() => {
+          //
+        }}
+        onClose={() => setOpen(false)}
+      />
+    </div>
+  );
+}
+
+export default Teacher;
